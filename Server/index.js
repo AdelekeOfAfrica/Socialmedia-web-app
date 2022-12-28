4cccc704fb602c54f93fc65652c669085bef3641
adelekeofafrica <adelekeofafrica@gmail.com>
2022-12-28 20:01:06 +0100

    adding of dummy files

--- Server/index.js
+++ Server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import {register} from "./controllers/auth.js";
 import {createPost} from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Posts.js";
+import {users,posts} from "./data/index.js";
 
 
 /* CONFIGURATIONS */
@@ -65,6 +68,10 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(() =>{
     app.listen(PORT, ()=> console.log(`Server Port: ${PORT}`));
+
+    //manually inserting the code into the database please not you can oonly do this onces
+    User.insertMany(users);
+    Post.insertMany(posts);
 
 })
 .catch((error)=>console.log(`${error} did not connect `));
